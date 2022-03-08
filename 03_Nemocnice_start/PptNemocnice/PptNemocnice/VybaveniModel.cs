75b572ee6f99995b3cd24992eb21f1d0de9fe479
david-pilny <93579404+david-pilny@users.noreply.github.com>
2022-03-08 16:22:23 +0100

    upravy

---
+++ 03_Nemocnice_start/PptNemocnice/PptNemocnice/VybaveniModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace PptNemocnice
+{
+	public class VybaveniModel
+	{
+		public string? Name { get; set; }
+
+		public DateTime BoughtDate { get; set; }
+
+		public DateTime LastRevisionDate { get; set; }
+
+		public bool NeedsRevision;
+	}
+}
+
