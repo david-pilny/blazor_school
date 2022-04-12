1ff1bc5dda3a625989929d6413c641716049f4cb
david-pilny <93579404+david-pilny@users.noreply.github.com>
2022-04-12 16:39:25 +0200

    Test09

---
+++ Projekt_nemocnice/PptNemocnice/PptNemocnice.Shared/RevizeModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace PptNemocnice.Shared
+{
+    public class RevizeModel
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; } = "";
+    }
+
+}
