81e916ba1c47f8eb2b512b12b6d92819a00041e8
david-pilny <93579404+david-pilny@users.noreply.github.com>
2022-05-10 17:27:27 +0200

    CORS 2

--- Projekt_nemocnice/PptNemocnice/PptNemocnice/Program.cs
+++ Projekt_nemocnice/PptNemocnice/PptNemocnice/Program.cs
@@ -3,9 +3,13 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PptNemocnice;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+var apiUri = builder.Configuration["ApiUri"];
+if (apiUri == null) throw new ArgumentNullException(nameof(apiUri));
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7186") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiUri) });
 
 await builder.Build().RunAsync();
